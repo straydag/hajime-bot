0cf6bb28022acf997ce732b9d2699875af93b0b7
carlo <carlovega20@gmail.com>
2020-02-19 10:19:21 -0800

    added a config file as well as removed existing token

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import json
 import discord
 from discord.ext import commands
+
+#grabs token from config file
+with open('config.json') as config_file:
+    token = json.load(config_file)["token"]
+
 
 client = discord.Client()
 
@@ -12,6 +18,4 @@ async def on_message(message):
     if message.content.startswith('hajime'):
         await message.channel.send('hello ' + message.author.name)
 
-
-client.run("Njc5MjEzNTg4NDI1OTk4MzM3.Xkvz3g.kOWAVZMkKo73ZIMf4c2pybfZgRk")
-
+client.run(token)
